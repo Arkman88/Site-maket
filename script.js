a9a4a50fe668b5b1b578cac27d54fc405d9ce19c
Burov Arkadii <arkman1988@gmail.com>
2024-05-05 21:06:41 +0300

    new slider menu #1

---
+++ script.js
@@ -0,0 +1,17 @@
+
+/*
+document.addEventListener('DOMContentLoaded', _ => {
+    document.getElementById('btn').addEventListener('click', function () {
+        document.getElementById('more').classList.toggle('off')
+        this.innerHTML = this.innerHTML === 'Скрыть' ? 'Читать далее' : 'Скрыть'
+    })
+})
+*/
+
+let tag = document.querySelector('swiper-container');
+let screenWidth = document.querySelector('html').offsetWidth;
+
+let gap = Number(tag.getAttribute('space-between'));
+let blockWidth = Number(document.querySelector('swiper-slide').clientWidth);
+let k = (screenWidth + gap) / (blockWidth + gap);
+tag.setAttribute('slides-per-view', k);
