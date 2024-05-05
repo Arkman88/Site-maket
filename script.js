8a399646fc895620fc64f6fe35beba975f4826c8
Burov Arkadii <arkman1988@gmail.com>
2024-05-05 21:33:03 +0300

    new swiper

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', _ => {
 })
 */
 
+/*
 let tag = document.querySelector('swiper-container');
 let screenWidth = document.querySelector('html').offsetWidth;
 
@@ -15,3 +16,4 @@ let gap = Number(tag.getAttribute('space-between'));
 let blockWidth = Number(document.querySelector('swiper-slide').clientWidth);
 let k = (screenWidth + gap) / (blockWidth + gap);
 tag.setAttribute('slides-per-view', k);
+*/
