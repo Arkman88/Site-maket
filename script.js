3266a17faa43c0fd0f1cf6c61d3eba38749a25ce
Arkady Burov <93515199+Arkman88@users.noreply.github.com>
2024-05-06 11:13:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-/*
-
-document.addEventListener('DOMContentLoaded', _ => {
-    document.getElementsByClassName('button-show-more').addEventListener('click', function () {
-        document.getElementsByClassName('button-show-more__text').classList.toggle('off')
-        this.innerHTML = this.innerHTML === 'Скрыть' ? 'Читать далее' : 'Скрыть'
-    })
-})
-
-*/
-/*
-let tag = document.querySelector('swiper-container');
-let screenWidth = document.querySelector('html').offsetWidth;
-
-let gap = Number(tag.getAttribute('space-between'));
-let blockWidth = Number(document.querySelector('swiper-slide').clientWidth);
-let k = (screenWidth + gap) / (blockWidth + gap);
-tag.setAttribute('slides-per-view', k);
-*/
+function readMore() {
+    let more = document.querySelector(".swiper__menu");
+    let btn = document.querySelector(".button-show-more");
+    if (more.style.overflow !== "visible" ) {
+        more.style.overflow = "visible";
+        more.style.height = "auto";        
+        btn.innerHTML = "Скрыть";    
+        btn.style.background = 'url(images/expand_alt.svg) no-repeat'
+        console.log(more);  
+    }
+    else {           
+        more.style.overflow = "hidden";
+        more.style.height = "175px";
+        btn.innerHTML = "Показать всё";
+        btn.style.background = 'url(images/expand.svg) no-repeat'
+        console.log(more);
+    }
+}
