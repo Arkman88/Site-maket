776a1bac93763caec28a20be01d4aba7e23aea98
Burov Arkadii <arkman1988@gmail.com>
2024-05-09 19:14:12 +0300

    переделал полностью свайпер.

--- script.js
+++ script.js
@@ -1,18 +1,53 @@
 function readMore() {
-    let more = document.querySelector(".swiper__menu");
+    let more = document.querySelector(".swiper-wrapper");
     let btn = document.querySelector(".button-show-more");
     if (more.style.overflow !== "visible" ) {
         more.style.overflow = "visible";
         more.style.height = "auto";        
         btn.innerHTML = "Скрыть";    
         btn.style.background = 'url(images/expand_alt.svg) no-repeat'
-        console.log(more);  
     }
     else {           
         more.style.overflow = "hidden";
-        more.style.height = "175px";
+        more.style.height = "173px";
         btn.innerHTML = "Показать всё";
         btn.style.background = 'url(images/expand.svg) no-repeat'
-        console.log(more);
     }
-}
+  }
+
+  
+
+window.addEventListener('DOMContentLoaded', () => {
+    const resizableSwiper = (breakpoint, swiperClass, swiperSettings) => {
+    let swiper;
+    breakpoint = window.matchMedia(breakpoint);    
+    const enableSwiper = function(className, settings) {
+        swiper = new Swiper(className, settings);
+    }
+
+    const checker = function() {
+            if (breakpoint.matches) {
+                return enableSwiper(swiperClass, swiperSettings);
+            } else {
+                if (swiper !== undefined) swiper.destroy(true, true);
+                return;
+            }
+    };
+
+    breakpoint.addEventListener('change', checker);
+    checker();
+    }
+  
+    resizableSwiper(
+      '(max-width: 768px)',
+      '.swiper',
+      {
+        spaceBetween: 30,
+        slidesPerView: 'auto',
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+      },
+    );
+  });
