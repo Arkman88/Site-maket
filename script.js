82e0a55eb2796d32844a0cf2de06a69d203ad041
Burov Arkadii <arkman1988@gmail.com>
2024-05-09 19:26:43 +0300

    пофиксил кнопку показать всё и выравнивание space-around

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function readMore() {
 window.addEventListener('DOMContentLoaded', () => {
     const resizableSwiper = (breakpoint, swiperClass, swiperSettings) => {
     let swiper;
+    let btn = document.querySelector(".button-show-more");
     breakpoint = window.matchMedia(breakpoint);    
     const enableSwiper = function(className, settings) {
         swiper = new Swiper(className, settings);
@@ -27,6 +28,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const checker = function() {
             if (breakpoint.matches) {
+                btn.innerHTML = "Показать всё";
                 return enableSwiper(swiperClass, swiperSettings);
             } else {
                 if (swiper !== undefined) swiper.destroy(true, true);
